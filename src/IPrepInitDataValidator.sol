d76e89b1d63a4abc9a223e1631a411a77f47d08d
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2025-02-25 11:19:44 +0300

    add interface

---
+++ src/IPrepInitDataValidator.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+interface IPrepInitDataValidator {
+    function validatePREPInitData(bytes calldata prepInitData) external view returns (bytes calldata initData);
+}
