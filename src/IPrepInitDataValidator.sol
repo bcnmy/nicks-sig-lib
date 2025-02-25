c4e98c6a518e7a3c9dbbac6f2d6691a3a9352d39
Filipp Makarov <fmc@Filipps-MacBook-Pro.local>
2025-02-25 11:34:08 +0300

    rm return var name

--- src/IPrepInitDataValidator.sol
+++ src/IPrepInitDataValidator.sol
@@ -2,5 +2,5 @@
 pragma solidity ^0.8.20;
 
 interface IPrepInitDataValidator {
-    function validatePREPInitData(bytes calldata prepInitData) external view returns (bytes calldata initData);
+    function validatePREPInitData(bytes calldata prepInitData) external view returns (bytes calldata);
 }
